62c11fa0ef0e251dfe91b0cc5508477ee78b770e
Mainafrancis <francis.maina@student.moringaschool.com>
2022-03-11 21:07:32 +0300

    config js

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ $(document).ready(function(){
     $("#desText").slideDown('1500').hide('1000');
     $("#design").show('1500');
 });
+$("#design").click(function(){
+    $("#design").slideUp('1500');
+    $("#desImg").slideDown('1500');
+    $("#desText").slideDown('1500');
+});
